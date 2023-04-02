57ed580b4060e5dee680e22c79a1c185911c7514
Nikita Berstnev <bignick666@yandex.ru>
2023-04-02 17:34:05 +0300

    hzhzzz

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, permissions
+from rest_framework.exceptions import PermissionDenied
 
 from posts.models import Post, Group
 from .serializers import PostSerializer,\
@@ -9,6 +10,14 @@ from .serializers import PostSerializer,\
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(PostViewSet, self).perform_update(serializer)
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
