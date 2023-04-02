5e39fe227489a012ffcb8151f4adaa27c06fa914
Nikita Berstnev <bignick666@yandex.ru>
2023-04-02 17:25:20 +0300

    hzhzzz

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -3,6 +3,11 @@ from posts.models import Post, Group
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
     class Meta:
         fields = ('id', 'text', 'author', 'image',
                   'pub_date', 'image')
@@ -16,5 +21,10 @@ class GroupSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
     class Meta:
         fields = ('id', 'author', 'post', 'text', 'created')
