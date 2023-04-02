0b101c4bb97caaa157b590cbe949da46d5edee65
Nikita Berstnev <bignick666@yandex.ru>
2023-04-02 17:04:04 +0300

    hzhzzz

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from posts.models import Post, Group
 
 
-
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'text', 'author', 'image',
