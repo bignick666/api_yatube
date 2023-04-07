18eace476d2a214d844c7713fd5aa6159535b186
Nikita Berstnev <bignick666@yandex.ru>
2023-04-07 21:36:09 +0300

    asdasd

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -11,7 +11,8 @@ from .serializers import (PostSerializer,
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (AuthorOrReadOnly,)
+    permission_classes = (AuthorOrReadOnly,
+                          permissions.IsAuthenticated)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
